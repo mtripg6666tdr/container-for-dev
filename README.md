213ca576fbf7ad4f75104f419e72ddc2eba507b1
うさみょん/myon2019 <56076195+mtripg6666tdr@users.noreply.github.com>
2025-03-21 18:43:44 +0900

    mount current directory

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Disposable Ubuntu image for my own personal development use
 ### Windows
 1. Place `ubuntu-dev.bat` somewhere and add its directory path to sysmte PATH.
 2. Open or restart your terminal and type `ubuntu-dev` to start using a dedicated virtual development environment.
+3. If you want to mount the current directory, please use `--mount` option and you can find the files in `/workspace/mount`.
 ### Linux
 *Coming Soon*
--- ubuntu-dev.bat
+++ ubuntu-dev.bat
@@ -1,4 +1,17 @@
 @echo off
 
+set MOUNT=0
+
+for %%a in (%*) do (
+  if "%%a"=="--mount" (
+    set MOUNT=1
+  )
+)
+
 docker pull ghcr.io/mtripg6666tdr/ubuntu-for-dev:latest
-docker run --rm -it ghcr.io/mtripg6666tdr/ubuntu-for-dev
+
+if %MOUNT%==1 (
+  docker run --rm -it --mount=type=bind,source=.,target=/workspace/mount ghcr.io/mtripg6666tdr/ubuntu-for-dev
+) else (
+  docker run --rm -it ghcr.io/mtripg6666tdr/ubuntu-for-dev
+)
